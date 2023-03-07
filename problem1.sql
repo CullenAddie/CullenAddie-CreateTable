22292d5c7acc2afc8b71e6a680a16d7efc9f4309
CullenAddie <cullenaddie1@gmail.com>
2023-03-07 03:07:09 +0000

    Cullen A

--- problem1.sql
+++ problem1.sql
@@ -1,5 +1,5 @@
-CREATE TABLE song (
-    column1 "Title" VARCHAR(100),
-    column2 "Artist" VARCHAR(100),
-   );
-   INSERT song values ('Let it Be', 'Beatles')
+create table song(
+  title varchar(100) primary key, artist VARCHAR(100)
+ 
+);
+insert into song(title, artist) values ('let it be', 'Beatles');
